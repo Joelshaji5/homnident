c55c5b26be75077733ad5f178b1b9faa21875876
joelshaji5 <joel.shaji15@gmail.com>
2024-01-04 23:47:34 +0530

    First commit

---
+++ src/app/Homepage/Hero2.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "../Style/Hero2.css"
+
+function Hero2() {
+  return (
+    <div className='flex justify-center'>
+
+    <div>
+        <img src="./hero2Bg.png" alt="" />
+        </div>
+    <div className='Hero2ParaDiv'>
+        <h2 className='Hero2Heading'>Who are we</h2>
+        <p  className='Hero2Para'>We’re a team of dentists who provide dental care under expert dentists, at your home, keeping in mind of our patient’s needs, convenience and safety. We believe in long term and personalized dental care instead of short-term solutions. So, we provide distinguished, ethical and informed dental care. We also use the best and most advanced dental technology. We aim to deliver high 
+        quality and effective dental treatments at affordable rates to all our patients. We provide extensive dental services.</p>
+    </div>
+
+
+
+    </div>
+  )
+}
+
+export default Hero2
